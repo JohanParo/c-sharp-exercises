bcbac4f4c7c3530620aeee07ca4448853eab23e5
johan <jparo@kth.se>
2022-11-01 15:40:36 +0100

    version 1

---
+++ c-sharp-exercises/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_exercises
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Pay { get; set; }
+
+        public Employee(string name, int pay)
+        {
+            this.Name = name;
+            this.Pay = pay;
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public int GetPay()
+        {
+            return this.Pay;
+        }
+    }
+}
--- c-sharp-exercises/Program.cs
+++ c-sharp-exercises/Program.cs
@@ -4,7 +4,64 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             Uppgift 1:
+                Klasser: Employee
+             */
+            /*
+             Uppgift 2:
+                Attribut:
+                    Namn, Lön
+                Metoder:
+                    GetName, GetPay, PrintRegistery
+             */
+
+            //Uppgift 3:
+
+            int end = 1;
+            LinkedList<Employee> employees = new LinkedList<Employee>();
+
+            employees.AddLast(new Employee("Johan Parö", 150));
+            employees.AddLast(new Employee("Erik Parö", 140));
+
+
+            while (end != 0)
+            {
+                Console.WriteLine("To add a employee type 1, o show the registery type 2, to exit type 0.");
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+
+                    Console.Write("Skriv in personens namn: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Skriv in personens lön: ");
+                    int pay = Int32.Parse(Console.ReadLine());
+
+                    employees.AddLast(new Employee(name, pay));
+                }
+                else if (input == "2")
+                {
+                    PrintRegistry(employees);
+                }
+                else if (input == "0")
+                {
+                    end = 0;
+                }
+            }
         }
+
+        private static void PrintRegistry(LinkedList<Employee> list)
+        {
+            Console.WriteLine("Register över anställda");
+            foreach (Employee employee in list)
+            {
+                string msg = "Namn: " + employee.GetName() + ", Lön: " + employee.GetPay() + "kr.";
+                Console.WriteLine(msg);
+            }
+            Console.WriteLine();
+        }
+
     }
 }
