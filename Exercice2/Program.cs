feaa0c61dff67152319094b73b58c395d9344b14
johan <jparo@kth.se>
2022-11-03 16:49:32 +0100

    implemented the 4th menu option

--- Exercice2/Program.cs
+++ Exercice2/Program.cs
@@ -11,12 +11,13 @@
             Console.WriteLine("Welcome to the main menu!");
             do
             {
-                Console.WriteLine("----------------------------------------------------------------------");
+                Console.WriteLine(" --------------------------------------------------------------------");
                 Console.WriteLine("| För att komma till Biljettpriser tryck 1.                          |");
                 Console.WriteLine("| För att räkna ut biljettpris för flera personer tillsammans tryck 2|");
                 Console.WriteLine("| För att upprepa en inskriven text 10 gånger tryck 3                |");
-                Console.WriteLine("| to do fourth thing type 4, to quit type 0.                         |");
-                Console.WriteLine("----------------------------------------------------------------------");
+                Console.WriteLine("| För att få tillbaka det tredje ordet i en inskriven mening tryck 4 |");
+                Console.WriteLine("| För att avsluta programmet tryck 0.                                |");
+                Console.WriteLine(" --------------------------------------------------------------------");
                 string input = Console.ReadLine();
 
                 switch (input)
@@ -26,9 +27,9 @@
                             keepGoing = false;
                             break;
                         }
-                    case "1": //check age of customer
+                    case "1": //check age of customer and return price
                         {
-                            Console.WriteLine("Ange din ålder");
+                            Console.WriteLine("Ange din ålder:");
                             int age = Int32.Parse(Console.ReadLine());
                             if (age < 20)
                             {
@@ -44,9 +45,8 @@
                             }
                             break;
                         }
-                    case "2":
+                    case "2": //check price of many different people and return total price
                         {
-
                             Console.WriteLine("Hur många personer är ni?");
                             int customers = Int32.Parse(Console.ReadLine());
                             int totalprice = 0;
@@ -60,10 +60,9 @@
                             Console.WriteLine($"Den totala kostnaden för er är {totalprice}kr");
                             break;
                         }
-                    case "3":
+                    case "3": //repeat input text 10 times
                         {
-
-                            Console.WriteLine("Skriv in en godtycklig text");
+                            Console.WriteLine("Skriv in en godtycklig text:");
                             string text = Console.ReadLine();
 
                             for (int i = 0; i < 10; i++)
@@ -79,8 +78,21 @@
                             }
                             break;
                         }
-                    case "4":
+                    case "4": //return the third word in a user input sentence
                         {
+                            Console.WriteLine("Skriv in en mening och få tillbaka det tredje ordet i meningen:");
+                            string textInput = Console.ReadLine();
+
+                            List<string> textList = new List<string>();
+                            string[] strings = textInput.Split(" ");
+                            if (strings.Length < 3)
+                            {
+                                Console.WriteLine("Det måste finnas minst 3 ord i meningen");
+                            }
+                            else
+                            {
+                                Console.WriteLine(strings[2]);
+                            }
                             break;
                         }
                     default:
@@ -89,10 +101,10 @@
                             break;
                         }
                 }
-
             } while (keepGoing);
         }
 
+        //Get the price of the ticket based on input age
         private static int GetTicketPrice(int age)
         {
             if (age < 20)
