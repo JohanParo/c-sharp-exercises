90db012faf18650ceed463f474c055e9bab15240
johan <jparo@kth.se>
2022-11-08 16:46:12 +0100

    added usererror and input error classes

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -5,11 +5,19 @@
         static void Main(string[] args)
         {
             PersonHandler personHandler = new PersonHandler();
-
             Person person1 = personHandler.CreatePerson(28, "Johan", "Paro", 194, 130);
-
-
             Console.WriteLine(person1.ToString());
+
+
+            List<UserError> userErrors = new List<UserError>();
+
+            userErrors.Add(new TextInputError());
+            userErrors.Add(new NumericInputError());
+
+            foreach (UserError error in userErrors)
+            {
+                Console.WriteLine(error.UEMessage());
+            }
 
         }
     }
