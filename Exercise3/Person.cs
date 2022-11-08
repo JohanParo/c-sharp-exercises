2808d318de5fa9b2e03e77c163e9f86aee47dad8
johan <jparo@kth.se>
2022-11-08 15:27:44 +0100

    Added Person

---
+++ Exercise3/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class Person
+    {
+
+        private int age;
+        private string firstName;
+        private string lastName;
+        private int height;
+        private int weight;
+
+        public int Age
+        {
+            get { return age; }
+            set 
+            {
+                if (age > 0) { age = 0; }
+                age = value; 
+            }
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set { 
+                if (firstName.Length < 2 || firstName.Length > 10) { throw new ArgumentException("Not a valid input","firstName"); }
+                firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set {
+                if (lastName.Length < 2 || lastName.Length > 10) { throw new ArgumentException("Not a valid input", "lastName"); }
+                lastName = value; }
+        }
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+
+
+    }
+}
