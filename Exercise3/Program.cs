793c9003d6535f5fa8cc246f5bba68cfadf27fe1
johan <jparo@kth.se>
2022-11-09 15:57:24 +0100

    3.3 klar

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -2,6 +2,16 @@
 {
     internal class Program
     {
+        /*
+         Frågor
+        3.3
+            13: Om vi upptäcker att alla fåglar behöver ett nytt attribut så lägger vi det i Bird klassen.
+            14: Om alla djur behöver få ett nytt attribut så lägger vi det i Animal klassen.
+         
+         3.4
+
+         
+         */
         static void Main(string[] args)
         {
             PersonHandler personHandler = new PersonHandler();
---
+++ Exercise3/Wolfman.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class Wolfman : Wolf, IPerson
+    {
+        public Wolfman(string name, int age) : base(name, age)
+        {
+            base.Age = age;
+            base.Name = name;
+        }
+
+        public void Talk()
+        {
+            Console.WriteLine("You gonna eat that?");
+        }
+    }
+}
