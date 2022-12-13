f29539514a99a1ac9298650f78873ada37f9b464
johan <jparo@kth.se>
2022-12-13 10:18:56 +0100

    now takes user input

--- Exercise9/script.js
+++ Exercise9/script.js
@@ -1,8 +1,13 @@
 
 const btn1 = document.querySelector('#btn1');
+const formBtn = document.querySelector('#form-submit');
+const form = document.querySelector('#bishbosh-form');
+const bishNumber = document.querySelector('#bish-number');
+const boshNumber = document.querySelector('#bosh-number');
+const totalCount = document.querySelector('#count-number');
 
-function checkIfBish(input) {
-    if ((input%3) === 0) {
+function checkIfBish(input, bishnumber) {
+    if ((input%bishnumber) === 0) {
         return true;
     }
     else {
@@ -10,8 +15,8 @@ function checkIfBish(input) {
     }
 };
 
-function checkIfBosh(input) {
-    if ((input%4) === 0) {
+function checkIfBosh(input, boshnumber) {
+    if ((input%boshnumber) === 0) {
         return true;
     }
     else {
@@ -22,15 +27,36 @@ function checkIfBosh(input) {
 btn1.addEventListener('click', function(e){
     for (let i = 100; i > 0; i--) {
         
-        if(checkIfBish(i)) {
-            if (checkIfBosh(i)) {
+        if(checkIfBish(i, 3)) {
+            if (checkIfBosh(i, 4)) {
                 console.log('Bish-Bosh');
             }
             else {
                 console.log('Bish');
             }
         }
-        else if (checkIfBosh(i)) {
+        else if (checkIfBosh(i, 4)) {
+            console.log('Bosh');
+        }
+        else {
+            console.log(i);
+        }
+    }
+});
+
+formBtn.addEventListener('click', function(e) {
+    console.log('yep');
+    //console.log(totalCount.value);
+    for (let i = totalCount.value; i > 0; i--) {
+        if (checkIfBish(i, bishNumber.value)) {
+            if (checkIfBosh(i, boshNumber.value)) {
+                console.log('Bish-Bosh');
+            }
+            else {
+                console.log('Bish')
+            }
+        }
+        else if (checkIfBosh(i, boshNumber.value)) {
             console.log('Bosh');
         }
         else {
